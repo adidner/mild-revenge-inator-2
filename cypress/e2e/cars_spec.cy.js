b08be4a99c10aa344ab3c5e2fef2acc23c418b80
Aaron Didner <adidner@gmail.com>
2022-06-19 15:23:40 -0400

    more promising updates

--- cypress/e2e/cars_spec.cy.js
+++ cypress/e2e/cars_spec.cy.js
@@ -1,5 +1,15 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.carfax.com/Used-Cars-in-Baltimore-MD_c1115');
+  // it('Car Attack 1', () => {
+  //   cy.visit('https://www.fitzmall.com/Inventory/SearchResults?Conditions=ALL&Makes=&UseCriteria=true');
+  // })
+  it('Car Attack 2', () => {
+    cy.visit('https://www.enterprisecarsales.com/list/used-cars-maryland/');
+    cy.contains("Elantra").click();
   })
+  // it('Car Attack 3', () => {
+  //   cy.visit('https://www.cars.com/shopping/baltimore-md/');
+  //   // let allClickCards = cy.get('[class="vehicle-card-main js-gallery-click-card"]');
+  //   let allClickCards = cy.contains("2021");
+  //   allClickCards.click();
+  // })
 })
